42453c4658365d3f14b721001bdee2a93f07a2d6
Blahblah733 <jasonlane@live.com.au>
2025-09-03 16:14:29 +1000

    Squashed commit of the following:
    commit b18b31a668760d2b17f4f4009b0c1bf9d466a345
    Author: Blahblah733 <jasonlane@live.com.au>
    Date:   Wed Sep 3 16:12:21 2025 +1000
    
        Changed Scene Name
    
    commit a52e5d7901727f76a825ceb6f2f2029685cbf75e
    Author: Blahblah733 <jasonlane@live.com.au>
    Date:   Tue Sep 2 17:15:29 2025 +1000
    
        Lockpick Puzzle Complete
    
        I have made a lockpick / Time Alignment puzzle for the game

---
+++ Assets/Scripts/PinManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PinManager : MonoBehaviour
+{
+    public PinMovement[] pins;          // assign in Inspector (the 6 UI pins)
+    public RectTransform targetLine;    // the yellow line (UI Image)
+    public float tolerance = 1500f;       // pixels
+    public GameObject failText;
+    public GameObject completeText;
+
+    int currentIndex = 0;
+
+    void Awake()
+    {
+        // HARD stop all pins so Inspector values can’t make them run
+        foreach (var p in pins)
+            if (p != null) p.Halt();
+        if (failText != null)
+            failText.SetActive(false);
+        if (completeText != null) 
+            completeText.SetActive(false);
+    }
+
+    void Start()
+    {
+        ActivateCurrentPin();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            HandleClick();
+    }
+
+    void HandleClick()
+    {
+        if (currentIndex >= pins.Length) return;
+
+        var pin = pins[currentIndex];
+        pin.Halt(); // freeze the current one
+
+        float distance = Mathf.Abs(pin.transform.position.y - targetLine.position.y);
+
+        if (distance <= tolerance)
+        {
+            Debug.Log($"Pin {currentIndex} SUCCESS!");
+            currentIndex++;
+            if (currentIndex < pins.Length)
+                ActivateCurrentPin();
+            else
+            {
+                Debug.Log("Puzzle Complete!");
+                if (completeText != null)
+                    completeText.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log($"Pin {currentIndex} FAIL!");
+            StartCoroutine(ShowFailAndRestart());
+        }
+    }
+
+    void ActivateCurrentPin()
+    {
+        // ensure only the current pin is moving
+        for (int i = 0; i < pins.Length; i++)
+        {
+            if (pins[i] == null) continue;
+            if (i == currentIndex) pins[i].Begin();
+            else pins[i].Halt();
+        }
+    }
+
+    IEnumerator ShowFailAndRestart()
+    {
+        if (failText != null)
+            failText.SetActive(true);
+
+        yield return new WaitForSeconds(2f); // show FAIL for 2 seconds
+
+        if (failText != null)
+            failText.SetActive(false);
+
+        RestartPuzzle();
+    }
+
+    void RestartPuzzle()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
