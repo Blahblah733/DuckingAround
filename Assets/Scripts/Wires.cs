efaa1753c7e765ad08c2e6fa650cfe830c28866b
Minii23 <Milly.Walker3581@gmail.com>
2025-09-04 16:44:06 +1000

    Script to drag and move wires
    Wire does not drag in right direction

--- Assets/Scripts/Wires.cs
+++ Assets/Scripts/Wires.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Wires : MonoBehaviour
 {
-    public void Blue()
+    public SpriteRenderer wireEnd;
+    Vector3 startPoint;
+    Vector3 startPosition;
+
+    private void Start()
     {
-        
+        startPoint = transform.parent.position;
+        startPosition = transform.position;
     }
 
-    public void Red()
+    private void OnMouseDrag()
     {
+        // Mouse position for world point
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0;
 
+        // Checking for nearby connection points
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                UpdateWire(collider.transform.position);
+            }
+        }
+
+        // Update Wire
+        UpdateWire(newPosition);
+    }
+
+    private void OnMouseUp()
+    {
+        // Resetting wire position
+        UpdateWire(startPosition);
+    }
+
+    void UpdateWire(Vector3 newPosition)
+    {
+        // Updates position, direction and scale
+        transform.position = newPosition;
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2(dist, wireEnd.size.y);
     }
 }
