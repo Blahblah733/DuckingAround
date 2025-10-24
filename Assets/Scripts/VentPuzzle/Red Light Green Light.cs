accdd3529b2e6394df2f40b833fe7d61dac5f21d
Blahblah733 <jasonlane@live.com.au>
2025-10-24 22:17:00 +1100

    Made the Basis of the Vent Game
    Made an adaptation of red light green light where the player is inside a vent
    
    However currently it's just basic movement and a red light green light style script

--- Assets/Scripts/VentPuzzle/Red Light Green Light.cs
+++ Assets/Scripts/VentPuzzle/Red Light Green Light.cs
@@ -1,13 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RedLightGreenLight : MonoBehaviour
 {
-    private GameObject player;
+    public GameObject guardSpeach;
 
-    private void Start()
+    public GameObject guardWarn;
+
+    public string loseSceneName = "G-O Vent";
+
+    public float minWaitTime = 2f;
+
+    public float maxWaitTime = 5f;
+
+    [HideInInspector] public bool isGuardSpeaking = false;
+
+    void Start()
     {
-        
+        StartCoroutine(LightCycle());
     }
+
+
+    void Update()
+    {
+        if (isGuardSpeaking)
+        {
+            if (Input.GetAxisRaw("Horizontal") !=0)
+            {
+                Debug.Log("Player Moved During Guard Speaking");
+                SceneManager.LoadScene(loseSceneName);
+            }
+        }
+    }
+
+    IEnumerator LightCycle()
+    {
+        while (true)
+        {
+            // Safe to Go
+            isGuardSpeaking = false;
+            guardSpeach.SetActive(false);
+            guardWarn.SetActive(false);
+            Debug.Log("Good to Go!");
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+            // Warning Phase
+            Debug.Log("Stop soon");
+            guardWarn.SetActive(true);
+            yield return new WaitForSeconds(1.0f); // short warning before red light
+
+            // Not Safe
+            guardWarn.SetActive(false);
+            isGuardSpeaking =true;
+            guardSpeach.SetActive(true);
+            Debug.Log("Not Safe");
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+        }    
+    }
+
+
 }
