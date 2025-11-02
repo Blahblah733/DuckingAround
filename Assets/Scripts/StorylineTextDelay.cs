33d4a773c4e4404d89766c8c1b7e25264b87183c
Minii23 <Milly.Walker3581@gmail.com>
2025-11-02 16:38:18 +1100

    Fixed HTP scene. Built Starting Dialogue.

--- Assets/Scripts/StorylineTextDelay.cs
+++ Assets/Scripts/StorylineTextDelay.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class StorylineTextDelay : MonoBehaviour
 {
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+    public GameObject button;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text1.SetActive(false);
+        text2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Invoke("lineOne", 1);
+        Invoke("characterName", 9);
+        Invoke("lineTwo", 12);
+        Invoke("lineThree", 30);
+        Invoke("continueButton", 38);
+    }
+
+    void lineOne()
+    {
+        text1.SetActive(true);
+    }
+
+    void characterName()
+    {
+        text2.SetActive(true);
+    }
+
+    void lineTwo()
+    {
+        text1.SetActive(false);
+        text2.SetActive(false);
+        text3.SetActive(true);
+    }
+
+    void lineThree()
+    {
+        text3.SetActive(false);
+        text4.SetActive(true);
+    }
+
+    void continueButton()
+    {
+        button.SetActive(true);
     }
 }
